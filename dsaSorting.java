39c3687ca3949161095e5db3dfb24fc0a4dee87c
NakumJaydeep <jnakum0796@gmail.com>
2025-09-15 21:20:42 +0530

    for my work

---
+++ dsaSorting.java
@@ -0,0 +1,61 @@
+public class dsaSorting {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // time complexity O(n^2)
+
+        
+        //bubble sort
+        int arr[] ={9,6,4,8,2};
+        int temp=0;
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length-1; i++) {  // 5-1=4
+            for (int j = 0; j < arr.length-i-1; j++) {  //5-0-1 =4 5-1-1=3
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+
+        //selection sort
+        int arr1[] = {9,4,6,2,1};
+        System.out.println(arr1.length);
+
+        for (int i = 0; i < arr1.length-1; i++) {
+            for (int j = i+1; j < arr1.length; j++) {
+                 if(arr1[i]>arr1[j]){
+                    temp = arr1[i];
+                    arr1[i] = arr1[j];
+                    arr1[j] = temp;
+                }
+            }
+        }
+        printArray(arr1);
+
+
+       //insertion sort
+       int arr2[] = {7, 5, 1, 9, 2};
+       System.out.println(arr1.length);
+
+       for(int i=1; i<arr2.length; i++) {
+           int current = arr2[i];
+           int j = i - 1;
+               while(j >= 0 && arr2[j] > current) {
+                   //Keep swapping
+                   arr2[j+1] = arr2[j];
+                   j--;
+               }
+           arr2[j+1] = current;
+       }
+       printArray(arr2);
+    }
+}
